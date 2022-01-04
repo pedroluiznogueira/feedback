f237cedd7cefe5cbebda0e1dad0786aa3d1678f7
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-04 16:07:29 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
+import Header from './components/Header';
+
 function App() {
-
-    const comments = [
-        {id: 1, text: 'comment one'},
-        {id: 2, text: 'comment two'},
-    ];
-
-    const loading = false;
-    const showComments = true;
-
-    if (loading) return <h1>loading...</h1>
 
     return(
         <>
-            <h1>Dynamic Stuff</h1>
-
-            { showComments && (
-                <div> 
-                    <h1>Comments({comments.length})</h1>
-                    {comments.map((comment, index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}    
-                </div>
-            )}
+            <Header />
+            <h1>My App</h1>
         </>
     );
 }
