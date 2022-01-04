e1e968d856b5c69f5cf59e18111d9c2e23f12b24
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-04 16:29:35 -0300

    Using global state in App component to pass a list of feedbacks to each FeedbackItem component displayed for each feedback in the list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
+import { useState } from 'react';
+
+const feedbackData = [
+    { rating: 5, text: 'First feedback' },
+    { rating: 10, text: 'Second feedback' }
+];
 
 function App() {
+    const [feedbacks, setFeedback] = useState(feedbackData)
 
     return(
         <>
             <Header />
-            <FeedbackItem />
+            <div className="container">
+                {feedbacks.map((feedback) => (
+                    <FeedbackItem item={feedback} />
+                ))}
+            </div>
         </>
     );
 }
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -3,21 +3,10 @@ import { useState } from "react"
 
 function FeedbackItem({ item }) {
 
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('Example text')
-
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev + 1;
-    })
-    setText('Text changed')
-  }
-
   return (
     <div className='card'>
-      <div className='num-display'>{rating}</div>
-      <div className='text-display'>{text}</div>
-      <button onClick={handleClick}>Change State</button>
+      <div className='num-display'>{item.rating}</div>
+      <div className='text-display'>{item.text}</div>
     </div>
   )
 }
