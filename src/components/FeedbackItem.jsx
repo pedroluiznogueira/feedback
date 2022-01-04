81e8d39109b9ba78354f9b4ba414eb72ce774f26
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-04 17:25:08 -0300

    Adding delete icon

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react"
 import Card from './shared/Card';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { FaTimes } from 'react-icons/fa';
 
 function FeedbackItem({ item }) {
 
   return (
     <Card reverse={item.rating == 0 && true}>
       <div className='num-display'>{item.rating}</div>
+      <button className="close">
+        <FaTimes color='purple'/>        
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   )
