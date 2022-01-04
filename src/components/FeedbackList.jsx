83c52b28b95cba5435bbf0af604446dcb1759d06
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-04 16:54:14 -0300

    Using props children

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,9 @@
 import FeedbackItem from './FeedbackItem';
 
 function FeedbackList( {feedbacks} ) {
+    
+    if (!feedbacks || feedbacks.length == 0) 
+        return <p>No feedback yet</p>
     return(
         <>
             {feedbacks.map((feedback) => (
