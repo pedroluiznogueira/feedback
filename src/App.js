69c5e3856f97c4c9dc25d15f064c4722f1a0d192
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-04 16:31:50 -0300

    Externalizing feedback data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
 import { useState } from 'react';
-
-const feedbackData = [
-    { rating: 5, text: 'First feedback' },
-    { rating: 10, text: 'Second feedback' }
-];
+import feedbackData from './data/FeedbackData';
 
 function App() {
     const [feedbacks, setFeedback] = useState(feedbackData)
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,6 @@
+const feedbackData = [
+    { rating: 5, text: 'First feedback' },
+    { rating: 10, text: 'Second feedback' }
+];
+
+export default feedbackData;
