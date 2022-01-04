527e9c126bc5a56f3393a78218bd3cb0b3ba6260
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-04 15:53:33 -0300

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,12 +5,24 @@ function App() {
         {id: 2, text: 'comment two'},
     ];
 
+    const loading = false;
+    const showComments = true;
+
+    if (loading) return <h1>loading...</h1>
+
     return(
-        <div>
-            {comments.map((comment, index) => (
-                <li key={index}>{comment.text}</li>
-            ))}    
-        </div>
+        <>
+            <h1>Dynamic Stuff</h1>
+
+            { showComments && (
+                <div> 
+                    <h1>Comments({comments.length})</h1>
+                    {comments.map((comment, index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}    
+                </div>
+            )}
+        </>
     );
 }
 
