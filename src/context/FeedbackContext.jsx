84e5ada9257b49ae5d602a3d723e6a6ba1f074ca
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-01-04 23:55:37 -0300

    adding a proxy to package.json

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -14,7 +14,7 @@ export const FeedbackProvider = ( {children} ) => {
 
     const fetchFeedback = async () => {
         setIsLoading(true);
-        const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc');
+        const response = await fetch('/feedback?_sort=id&_order=desc');
         const data = await response.json();
         setTimeout(() => {
             setFeedbacks(data);
